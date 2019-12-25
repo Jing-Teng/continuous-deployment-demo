9bf225e2ca3d4b50d4536717acf6131f43d9d406
JinTeng <53475827+Jing-Teng@users.noreply.github.com>
2019-12-26 00:13:10 +0800

    Update fetch_gae_sdk.py

--- scripts/fetch_gae_sdk.py
+++ scripts/fetch_gae_sdk.py
@@ -1,1 +1,23 @@
-test
+language: python
+sudo: required
+dist: xenial
+python: "3.7"
+
+before_install:
+  - openssl aes-256-cbc -K $encrypted_XXX_key -iv $encrypted_XXX_iv -in credentials.tar.gz.enc -out credentials.tar.gz -d
+  - tar -xzf credentials.tar.gz
+  - mkdir -p lib
+
+install:
+  - pip install -r requirements.txt
+  - pip install -r requirements-for-testing.txt
+
+script:
+  - pytest && pytest test_e2e/run.py
+
+deploy:
+  provider: gae
+  skip_cleanup: true # Skip cleanup so api_key.py and vendored dependencies are still there
+  keyfile: client-secret.json
+  project: project-name
+  default: true  # default project app version
